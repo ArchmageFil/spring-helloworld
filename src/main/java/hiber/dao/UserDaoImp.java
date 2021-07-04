6e0cb2842e61f3cc49db49e6aa7ad9544fb9342f
Archmage <archmagefil@gmail.com>
2021-07-04 13:01:43 +0300

    done

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,7 +1,9 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,19 +13,31 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession()
+                .createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User getByCar(Car car) {
+        Query<User> query = sessionFactory.getCurrentSession().createQuery(
+                "SELECT u from User u where u.car.model = :carModel and " +
+                        "u.car.series = :carSeries", User.class);
+        query.setParameter("carModel", car.getModel())
+                .setParameter("carSeries", car.getSeries());
+        return query.setMaxResults(1).getSingleResult();
+    }
 
 }
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -2,60 +2,87 @@ package hiber.model;
 
 import javax.persistence.*;
 
+@SuppressWarnings({"unused", "JpaDataSourceORMInspection"})
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @OneToOne(fetch = FetchType.EAGER, orphanRemoval = true, cascade = CascadeType.ALL)
+    @JoinColumn(name = "car_id", unique = true)
+    private Car car;
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    public User() {
+    }
 
-   @Column(name = "name")
-   private String firstName;
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-   @Column(name = "last_name")
-   private String lastName;
+    public User(String firstName, String lastName, String email, Car car) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.car = car;
+    }
 
-   @Column(name = "email")
-   private String email;
+    public Long getId() {
+        return id;
+    }
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public Long getId() {
-      return id;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public Car getCar() {
+        return car;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", car=" + car +
+                '}';
+    }
 }
